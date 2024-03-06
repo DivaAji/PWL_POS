fc45eb78aed209e02b3e21a93069caf0b1c5c149
DivaAji <diva7aji7@gmail.com>
2024-03-06 08:04:41 +0700

    Perbaikan Kode Barang & Kategori

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -16,16 +16,16 @@ class BarangSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['barang_id' => '1','kategori_id' => '1','barang_kode' => 'BRG001', 'barang_nama' => 'Kipas Angin', 'harga_beli' => 80000  , 'harga_jual' => 100000],
-            ['barang_id' => '2','kategori_id' => '1','barang_kode' => 'BRG002',  'barang_nama' => 'Antena', 'harga_beli' => 150000  , 'harga_jual' => 160000],
-            ['barang_id' => '3','kategori_id' => '2','barang_kode' => 'BRG003',  'barang_nama' => 'T-Shirt', 'harga_beli' => 80000 , 'harga_jual' =>100000],
-            ['barang_id' => '4','kategori_id' => '2','barang_kode' => 'BRG004',  'barang_nama' => 'Celana Jeans', 'harga_beli' => 50000 , 'harga_jual' => 65000],
-            ['barang_id' => '5','kategori_id' => '3','barang_kode' => 'BRG005',  'barang_nama' => 'Pensil', 'harga_beli' => 1000 , 'harga_jual' => 2000],
-            ['barang_id' => '6','kategori_id' => '3','barang_kode' => 'BRG006',  'barang_nama' => 'Bolpoin', 'harga_beli' => 2500 , 'harga_jual' =>4000],
-            ['barang_id' => '7','kategori_id' => '4','barang_kode' => 'BRG007',  'barang_nama' => 'Chiki', 'harga_beli' => 5000 , 'harga_jual' => 6500],
-            ['barang_id' => '8','kategori_id' => '4','barang_kode' => 'BRG008',  'barang_nama' => 'Roti', 'harga_beli' => 19500 , 'harga_jual' => 21000],
-            ['barang_id' => '9','kategori_id' => '5','barang_kode' => 'BRG009',  'barang_nama' => 'Teh Botol', 'harga_beli' => 6000 , 'harga_jual' => 7000],
-            ['barang_id' => '10','kategori_id' => '5','barang_kode' => 'BRG010',  'barang_nama' => 'Air Mineral', 'harga_beli' => 3000 , 'harga_jual' =>4000],
+            ['barang_id' => '1','kategori_id' => '1','barang_kode' => 'KPSANG', 'barang_nama' => 'Kipas Angin', 'harga_beli' => 80000  , 'harga_jual' => 100000],
+            ['barang_id' => '2','kategori_id' => '1','barang_kode' => 'ANTN',  'barang_nama' => 'Antena', 'harga_beli' => 150000  , 'harga_jual' => 160000],
+            ['barang_id' => '3','kategori_id' => '2','barang_kode' => 'TSHRT',  'barang_nama' => 'T-Shirt', 'harga_beli' => 80000 , 'harga_jual' =>100000],
+            ['barang_id' => '4','kategori_id' => '2','barang_kode' => 'JNS',  'barang_nama' => 'Celana Jeans', 'harga_beli' => 50000 , 'harga_jual' => 65000],
+            ['barang_id' => '5','kategori_id' => '3','barang_kode' => 'PNSL',  'barang_nama' => 'Pensil', 'harga_beli' => 1000 , 'harga_jual' => 2000],
+            ['barang_id' => '6','kategori_id' => '3','barang_kode' => 'BLPN',  'barang_nama' => 'Bolpoin', 'harga_beli' => 2500 , 'harga_jual' =>4000],
+            ['barang_id' => '7','kategori_id' => '4','barang_kode' => 'CHK',  'barang_nama' => 'Chiki', 'harga_beli' => 5000 , 'harga_jual' => 6500],
+            ['barang_id' => '8','kategori_id' => '4','barang_kode' => 'ROTI',  'barang_nama' => 'Roti', 'harga_beli' => 19500 , 'harga_jual' => 21000],
+            ['barang_id' => '9','kategori_id' => '5','barang_kode' => 'THBTL',  'barang_nama' => 'Teh Botol', 'harga_beli' => 6000 , 'harga_jual' => 7000],
+            ['barang_id' => '10','kategori_id' => '5','barang_kode' => 'AIRBTL',  'barang_nama' => 'Air Mineral', 'harga_beli' => 3000 , 'harga_jual' =>4000],
         ];
         DB::table('m_barang')->insert($data);
     }
--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -16,11 +16,11 @@ class KategoriSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['kategori_id' => '1','kategori_kode' => 'KTG001', 'kategori_nama' => 'Elektronik'],
-            ['kategori_id' => '2','kategori_kode' => 'KTG002', 'kategori_nama' => 'Pakaian'],
-            ['kategori_id' => '3','kategori_kode' => 'KTG003', 'kategori_nama' => 'Alat Tulis'],
-            ['kategori_id' => '4','kategori_kode' => 'KTG004', 'kategori_nama' => 'Makanan'],
-            ['kategori_id' => '5','kategori_kode' => 'KTG005', 'kategori_nama' => 'Minuman'],
+            ['kategori_id' => '1','kategori_kode' => 'ELKTRNK', 'kategori_nama' => 'Elektronik'],
+            ['kategori_id' => '2','kategori_kode' => 'PKIAN', 'kategori_nama' => 'Pakaian'],
+            ['kategori_id' => '3','kategori_kode' => 'ALTTLS', 'kategori_nama' => 'Alat Tulis'],
+            ['kategori_id' => '4','kategori_kode' => 'MKN', 'kategori_nama' => 'Makanan'],
+            ['kategori_id' => '5','kategori_kode' => 'MNM', 'kategori_nama' => 'Minuman'],
         ];
         DB::table('m_kategori')->insert($data);
     }
